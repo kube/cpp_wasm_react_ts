82b7f0497943ba6bd1935dc1bcff34b3808308e3
Chris Feijoo <kube@users.noreply.github.com>
2020-10-14 16:26:44 +0000

    SAVE

---
+++ .devcontainer/docker-entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /opt/emsdk/emsdk_env.sh
+
+exec "$@"
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
 import React, { FunctionComponent, useState } from 'react'
 import { render } from 'react-dom'
 
-const App: FunctionComponent = () => {
+function useCounter(initialValue = 0) {
   const [counter, setCounter] = useState(0)
-
+  
   const increment = () => setCounter(counter + 1)
   const decrement = () => setCounter(counter - 1)
 
+  return {
+    counter,
+    increment,
+    decrement
+  }
+}
+
+const App: FunctionComponent = () => {
+  const { counter, increment, decrement } = useCounter()
+
   return <div>
-    <h1>Hello</h1>
+    <h1>Hello WASM</h1>
     <p>{counter}</p>
 
     <button onClick={increment}>+</button>
